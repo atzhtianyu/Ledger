045e901d7863554777e7885a9bf38700ea5efa56
zhtianyu <1420098499@qq.com>
2021-03-07 16:39:54 +0800

    entity

---
+++ src/entity/Config.java
@@ -0,0 +1,31 @@
+package entity;
+
+public class Config {
+    public int id;
+    public String key;
+    public String value;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
